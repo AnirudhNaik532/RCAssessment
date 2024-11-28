7d005b7dc7fbeb5b8569044e5643825855fe72e8
Anirudh <anirudh@bestwing.co>
2024-11-28 14:46:45 +0400

    removed unnecessary comments

--- components/form/form-field.tsx
+++ components/form/form-field.tsx
@@ -32,7 +32,6 @@ export function DynamicFormField({
     onChange(fieldName, val);
   };
 
-  // Framer Motion animations
   const containerVariants = {
     hidden: { opacity: 0, y: -10 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
